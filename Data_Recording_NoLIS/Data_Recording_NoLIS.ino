f006ce6bea8f8942dfa96eca6263d7b1c0a821a1
Magician1427 <magician1427@gmail.com>
2022-04-01 21:13:32 -0400

    Version 1.1 Arduino Code

--- Data_Recording_NoLIS/Data_Recording_NoLIS.ino
+++ Data_Recording_NoLIS/Data_Recording_NoLIS.ino
@@ -12,15 +12,13 @@ int a1, a2, a3;
 
 //Average of 3 SM Sensor
 int average;
-int timer=0; //Timer as seconds to detect wether it is 20 minutes to store the data, and count every second to accumulate the data
-int sum=0; //Sum of every seconds of the average of SMS
-int sum2=0; //Sum of every seconds of the average of LIS
-int light = 0; //For blinkning the LED light 
-//For LI Sensor
+int timer = 0; //Timer as seconds to detect wether it is 20 minutes to store the data, and count every second to accumulate the data
+int sum = 0; //Sum of every seconds of the average of SMS
+int light = 0; //For blinkning the LED light
 
-byte buff[2];
+int period; //The period of recording data (period = 60 means every 60 seconds a data will be recorded)
 
-//Convert the data range from [a,b] to [c,d] (This is 
+//Convert the data range from [a,b] to [c,d]
 int numberConvertion(double old_value, double old_min, double old_max, double new_min, double new_max)
 {
   double new_value;
@@ -36,7 +34,6 @@ void setup()
   Serial.begin(9600);//init Serial rate
 
   pinMode(LED_BUILTIN, OUTPUT); //Set up the blinking the LED
-
 }
 
 void loop()
@@ -44,9 +41,9 @@ void loop()
   int i; //I don't know why here's an i either :(
   
   //Convert the SI Sensor's data into int[0,255] to store it
-  a1 = numberConvertion(analogRead(A1), 300, 600, 0, 255);
-  a2 = numberConvertion(analogRead(A2), 300, 600, 0, 255);
-  a3 = numberConvertion(analogRead(A3), 300, 600, 0, 255);
+  a1 = numberConvertion(analogRead(A1), 100, 1000, 0, 255);
+  a2 = numberConvertion(analogRead(A2), 100, 1000, 0, 255);
+  a3 = numberConvertion(analogRead(A3), 100, 1000, 0, 255);
   
 //  a1 = analogRead(A1);
 //  a2 = analogRead(A2);
@@ -63,7 +60,7 @@ void loop()
   Serial.println(a3);
   
   average = (a1 + a2 + a3) / 3; //Calculate the avergae of SMS since we only want to store the average
-  sum = sum +average; //Accumulate(Update) the sum of SMS
+  sum = sum + average; //Accumulate(Update) the sum of SMS
 
   //print on the computer for testing
   Serial.print("Average Soil Moisture: ");
@@ -74,33 +71,31 @@ void loop()
   //Letting the LED blink so that we can now the Arduino is Recording immediately
   if (light == 0){
     digitalWrite(LED_BUILTIN, HIGH);  
-    light++;  
+    light = 1 - light;
   }
   else if (light == 1){
     digitalWrite(LED_BUILTIN, LOW);
-    light=light-1;
+    light = 1 - light;
   }
 
   
-  delay(950); //Delay 1second 
+  delay(950 * 30); //Delay 30 seconds 
 
-  timer++; //add a second to the timer
-  if (timer >=60) //when 60 seconds past (should be 1200 seconds in experiment)
+  timer++; //add a count to the timer
+  if (timer >= 40) //when 20 minutes past (should be 1200 seconds in experiment)
   {
-    EEPROM.write(address++, sum2 / (60*60)); //Record Average of every seconds of LIS
-    EEPROM.write(address++, sum/60); //Only save average SM Sensor data
+    EEPROM.write(address++, sum / 40); //Only save average SM Sensor data
     
     //print on the computer for testing
-    Serial.print("Average of 5 seconds is:");
-    Serial.println(sum/60);
-    Serial.print("Average LIS of 5 seconds is:");
-    Serial.println(sum2 / (60*60));
+    Serial.print("Average of SMS in 60 seconds is:");
+    Serial.println(sum / 40);
     
     timer = 0; //clear the timer for next time's storing
     sum = 0; //clear the sum of SM for next time's storing
-    sum2 = 0; //clear the sum2 of LIS for next time's storing
   }
+
   Serial.println();
+
   if(address >1023)
   {
     while(1); //finish and stop recording
